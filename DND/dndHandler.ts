3d5d5250f45a0c2e4f44e6485d72105887c7cf12
JeongwooSeo <shipfriend4371@naver.com>
2024-09-07 16:25:52 +0900

    feat: 드래그앤드롭 세팅 완료

--- DND/dndHandler.ts
+++ DND/dndHandler.ts
@@ -1,8 +1,70 @@
 const dragStartHandler = (e: DragEvent) => {
-  const draggedItem = e.target;
+  const draggedItem = e.target as HTMLElement;
   if (draggedItem) {
     draggedItem.classList.add("place");
+    e.dataTransfer?.setData("text/plain", draggedItem.dataset.id || "");
   }
 };
 
-export { dragStartHandler };
+const dragEndHandler = (e: DragEvent) => {
+  const draggedItem = e.target as HTMLElement;
+  if (draggedItem) {
+    draggedItem.classList.remove("place");
+  }
+};
+
+const dragOverHandler = (e: DragEvent) => {
+  e.preventDefault();
+
+  // e.stopPropagation();
+
+  const target = e.target as HTMLElement;
+  const closest = target.closest(".drop-zone");
+  if (closest) {
+    const elements = [...closest.querySelectorAll(".item")] as HTMLElement[];
+    const index = findIndex(elements, e.y);
+    console.log(index);
+
+    // 미리보기 넣기
+    if (elements.length - 1 === index) {
+      closest.insertBefore(document.querySelector(".place"), null);
+    } else {
+      closest.insertBefore(document.querySelector(".place"), closest.children[index] || null);
+    }
+  }
+};
+
+const dropHandler = (e: DragEvent) => {
+  e.preventDefault();
+  const target = e.target as HTMLElement;
+  if (target) {
+    const closest = target.closest(".drop-zone") as HTMLElement;
+    const item = document.querySelector(
+      `.item[data-id="${e.dataTransfer?.getData("text/plain")}"]`
+    ) as HTMLElement;
+
+    if (closest) {
+      const index = findIndex([...closest.children] as HTMLElement[], e.y);
+      console.log(index);
+      if (index === 0) {
+        closest.insertBefore(item, (closest.firstElementChild as HTMLElement) || null);
+      } else {
+        closest.insertBefore(item, (closest.children[index] as HTMLElement) || null);
+      }
+    }
+  }
+};
+
+const findIndex = (elements: HTMLElement[], yPos: number) => {
+  // elements 들 중 yPOS가 몇번째 인덱스
+  const rects = elements.map((element) => {
+    const rect = element.getBoundingClientRect();
+    const center = rect.top + (rect.bottom - rect.top) / 2;
+    return center;
+  });
+  const index = rects.findIndex((rect) => rect >= yPos);
+
+  return index === -1 ? rects.length - 1 : index;
+};
+
+export { dragStartHandler, dragEndHandler, dragOverHandler, dropHandler };
--- DND/index.ts
+++ DND/index.ts
@@ -1,3 +1,6 @@
-import { dragStartHandler } from "./dndHandler";
+import { dragStartHandler, dragEndHandler, dropHandler, dragOverHandler } from "./dndHandler";
 
 document.addEventListener("dragstart", dragStartHandler);
+document.addEventListener("dragend", dragEndHandler);
+document.addEventListener("dragover", dragOverHandler);
+document.addEventListener("drop", dropHandler);
